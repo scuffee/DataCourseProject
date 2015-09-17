235d53ce25d1d205d9e94e2d41af74dc946dde12
scuffee <sc2714@att.com>
2015-09-17 08:23:30 -0500

    update project

---
+++ CourseProject/CodeBook.md
@@ -0,0 +1,50 @@
+the data was retreived from "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+The file was downloaded to R studio version 3.2.0ann unzipped to Windows 7 OS directory 
+C:/Users/Owner/Documents/specdata and the tables below were read.
+read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/activity_labels.txt")
+read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/features.txt")
+read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/test/x_test.txt")
+read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/test/y_test.txt", colClasses = "character")
+read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/train/x_train.txt")
+read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/train/y_train.txt", colClasses = "character")
+read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/test/subject_test.txt")
+read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/train/subject_train.txt")
+
+Labels 
+tableTestLabels  and tableTextLabels were converted from inter data to character data and combined into one table called tableCombineLabels and consisted of values 1 through 6. tableCombineLabels column V1 was renamed Activity and the values in the column were renamed as listed below:
+1=  WALKING
+2 =  WALKING_UPSTAIRS
+3=  WALKING_DOWNSTAIRS
+4=  SITTING
+5=  STANDING
+6=  LAYING
+
+Feature Selection 
+=================
+tableTest_X and tableTrain_X were combined in tableCombineX consists the test data of all 30 participants. A 561-feature vector with time and frequency domain variables. The features selected for this database come from the accelerometer and gyroscope 3-axial raw signals tAcc-XYZ and tGyro-XYZ. These time domain signals (prefix 't' to denote time) were captured at a constant rate of 50 Hz. Then they were filtered using a median filter and a 3rd order low pass Butterworth filter with a corner frequency of 20 Hz to remove noise. Similarly, the acceleration signal was then separated into body and gravity acceleration signals (tBodyAcc-XYZ and tGravityAcc-XYZ) using another low pass Butterworth filter with a corner frequency of 0.3 Hz.  
+
+tableSubjectTrain and tableSubjectTest were combined in tableCombineeSubjectID.  This table consisted of an identifier of the subject who carried out the experiment.  V1 was renamed TestSubjectID.  tablecombineLabels and tableCombineSubjectID were combined to create one data table called tableData.   
+
+
+A subset of 66 features were selected from tableData and renamed in tableDataScrub:  tBodyAccmeanX = V1, tBodyAccmeanY = V2, tBodyAccmeanZ = V3,
+                                          tBodyAccstdX = V4, tBodyAccstdY = V5, tBodyAccstdZ = V6,
+                                          tGravityAccmeanX = V41,tGravityAccmeanY = V42, tGravityAccmeanZ = V43,
+                                          tGravityAccstdX = V44, tGravityAccstdY = V45, tGravityAccstdZ = V46,
+                                          tBodyAccJerkmeanX = V81, tBodyAccJerkmeanY = V82, tBodyAccJerkmeanZ = V83,
+                                          tBodyAccJerkstdX = V84, tBodyAccJerkstdY = V85, tBodyAccJerkstdZ = V86,
+                                          tBodyGyromeanX = V121, tBodyGyromeanY = V122, tBodyGyromeanZ = V123,
+                                          tBodyGyrostdX = V124,tBodyGyrostdY = V125, tBodyGyrostdZ = V126,
+                                          tBodyGyroJerkmeanX = V161, tBodyGyroJerkmeanY = V162, tBodyGyroJerkmeanZ = V163,
+                                          tBodyGyroJerkstdX = V164, tBodyGyroJerkstdY = V165, tBodyGyroJerkstdZ = V166,
+                                          tBodyAccMagmean = V201, tBodyAccMagstd = V202, GravityAccMagmean = V214, tGravityAccMagstd = V215,
+                                          tBodyAccJerkMagmean = V227, tBodyAccJerkMagstd = V228, tBodyGyroMagmean = V240, tBodyGyroMagstd = V241,
+                                          tBodyGyroJerkMagmean = V253, tBodyGyroJerkMagstd = V254, fBodyAccmeanX = V266,fBodyAccmeanY = V267,
+                                          fBodyAccmeanZ = V268, fBodyAccstdX = V269, fBodyAccstdY = V270, fBodyAccstdZ = V271,
+                                          fBodyAccJerkmeanX = V345, fBodyAccJerkmeanY = V346, BodyAccJerkmeanZ = V347,
+                                          fBodyAccJerkstdX = V348, fBodyAccJerkstdY = V349, fBodyAccJerkstdZ = V350,
+                                          fBodyGyromeanX = V424, fBodyGyromeanY = V425, fBodyGyromeanZ = V426, fBodyGyrostdX = V427,
+                                          fBodyGyrostdY = V428, fBodyGyrostdZ = V429, fBodyAccMagmean = V503, fBodyAccMagstd = V504,
+                                          fBodyBodyAccJerkMagmean = V516, fBodyBodyAccJerkMagstd = V517, fBodyBodyGyroMagmean = V529,
+                                          fBodyBodyGyroMagstd = V530, fBodyBodyGyroJerkMagmean = V542, fBodyBodyGyroJerkMagstd = V543)
+  
+The data in tableDataScrub was arranged by the subject ID number and activity.  The data was also group by subject ID and activity, summarized using the mean of each activity per subject and written to text file tidydata.txt.
--- CourseProject/run_analysis.R
+++ CourseProject/run_analysis.R
@@ -4,14 +4,13 @@
 run_analysis <- function()
 {
  
-  library(httr)
-  library(utils)
   library(data.table)
   library(plyr)
   library(dplyr)
   library (tidyr)
   library(stringr)
   library(reshape2)
+  
 #-------------------------------------------------------------------------------------------------------  
 
 #Check to see if requred directory exists, if not create it.  
@@ -23,67 +22,74 @@ run_analysis <- function()
   
 #----------------------------------------------------------------------------------------------------------------------
 #Select file to be downloaded
- #fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+#fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 #Download the file to the console
-  #download.file(fileUrl, destfile = "C:/Users/Owner/Documents/specdata/2FUCI%20HAR%20Dataset.zip", method = "libcurl")
+#download.file(fileUrl, destfile = "C:/Users/Owner/Documents/specdata/2FUCI%20HAR%20Dataset.zip", method = "libcurl")
 #Record the date and time the file was downloaded
-  #dateDownLoaded <- date()
-  print("Date the files were downloaded:")
-  #print(dateDownLoaded)
-  #print(list.files("C:/Users/Owner/Documents/specdata"))
+#dateDownLoaded <- date()
+#print("Date the files were downloaded:")
+#print(dateDownLoaded)
+#print(list.files("C:/Users/Owner/Documents/specdata"))
 #Uzip the file
-  #unzip("C:/Users/Owner/Documents/specdata/2FUCI%20HAR%20Dataset.zip")
+#unzip("C:/Users/Owner/Documents/specdata/2FUCI%20HAR%20Dataset.zip")
   
 #----------------------------------------------------------------------------------------------------------------------
 #Read the data files
   tableActivityLabels <- read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/activity_labels.txt")
   tableFeatures <- read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/features.txt")
   tableTest_X <- read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/test/x_test.txt")
-  tableTestLabels<- read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/test/y_test.txt")
+  tableTestLabels<- read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/test/y_test.txt", colClasses = "character")
   tableTrain_X <- read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/train/x_train.txt")
-  tableTrainLabels <- read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/train/y_train.txt")
+  tableTrainLabels <- read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/train/y_train.txt", colClasses = "character")
   tableSubjectTest <- read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/test/subject_test.txt")
   tableSubjectTrain <- read.table("C:/Users/Owner/Documents/specdata/UCI HAR Dataset/train/subject_train.txt")
 #------------------------------------------------------------------------------------------------------------------
 #Merge the training and the test data to create one data set.
   tableCombineX <- rbind(tableTest_X, tableTrain_X) # combine the train and test data by
   tableCombineLabels <- rbind(tableTestLabels, tableTrainLabels) # combine train and test Activities by row
+  tableCombineLabels <- rename(tableCombineLabels, Activity = V1) #rename column with Test Subject Activity
+  tableCombineLabels$Activity <- gsub("1", "Walking", tableCombineLabels$Activity) #rename activity 1 walking
+  tableCombineLabels$Activity <- gsub("2", "WalkingUpStairs", tableCombineLabels$Activity) #rename activit 2 walkink up stairs
+  tableCombineLabels$Activity <- gsub("3", "WalkingDownStairs", tableCombineLabels$Activity) # rename activity 3 walkin down stairs
+  tableCombineLabels$Activity <- gsub("4", "Sitting", tableCombineLabels$Activity) # rename activity 4 sitting
+  tableCombineLabels$Activity <- gsub("5", "Standing", tableCombineLabels$Activity) # rename activity 5 standing
+  tableCombineLabels$Activity <- gsub("6", "Laying", tableCombineLabels$Activity) #vvrename activity 6 laying
   tableCombineSubjectID <- rbind(tableSubjectTest, tableSubjectTrain)# Combine test subject ID
-  tableCombineLabelsRename <- rename(tableCombineLabels, c("V1" = "Activity")) #rename column with Test Subject Activity
-  tableCombineSubjectIDRename <- rename(tableCombineSubjectID, c("V1" = "TestSubjectID"))#rename   coulm with Test Subject ID number
-  tableData <- cbind(tableCombineX, tableCombineLabelsRename, tableCombineSubjectIDRename) #data combined in one dataset
- 
+  tableCombineSubjectID <- rename(tableCombineSubjectID, TestSubjectID = V1)#rename   coulm with Test Subject ID number
+  tableData <- cbind(tableCombineX, tableCombineLabels, tableCombineSubjectID) #data combined in one dataset
+  
 #--------------------------------------------------------------------------------------------------
 #Extract the measurements on the mean and standard deviation for each measurement from the new dataset.
 
-   tableDataScrub <- select(tableData, c(TestSubjectID, Activity, 1:6, 41:46, 81:86, 121:126, 161:166, 201, 202, 214, 215, 227, 228, 240, 241, 253, 254, 266:271, 
-                                        345:350, 424:429, 503, 504, 516, 517, 529, 530, 542, 543))
+ tableDataScrub <- select(tableData, c(TestSubjectID, Activity, 1:6, 41:46, 81:86, 121:126, 161:166, 201, 202, 214, 215, 227, 228, 240, 241,253,254, 266:271, 
+                                                                345:350, 424:429, 503, 504, 516, 517, 529, 530, 542, 543))
  
-  #Rename the Activity and Features columns for clarification
+#Rename the Features columns for clarification
+
   
-  tableDataScrubTwo <- rename(tableDataScrub, c("V1" = "tBodyAcc-meanX", "V2" = "tBodyAcc-meanY", "V3" = "tBodyAcc-meanZ",
-                                                "V4" = "tBodyAcc-stdX", "V5" = "tBodyAcc-stdY", "V6" = "tBodyAcc-stdZ",
-                                                "V41" = "41 tGravityAcc-meanX", "V42" = "tGravityAcc-meanY", "V43" = "tGravityAcc-meanZ",
-                                                "V44" = "tGravityAcc-stdX", "V45" = "41 tGravityAcc-stdY", "V46" = "tGravityAcc-stdZ",
-                                                "V81" = "tBodyAccJerk-meanX", "V82" = "tBodyAccJerk-meanY", "V83" = "tBodyAccJerk-meanZ",
-                                                "V84" = "tBodyAccJerk-stdX", "V85" = "tBodyAccJerk-stdY", "V86" = "tBodyAccJerk-stdZ",
-                                                "V121" = "tBodyGyro-meanX", "V122" = "tBodyGyro-meanY", "V123" = "tBodyGyro-meanZ",
-                                                "V124" = "tBodyGyro-stdX","V125" = "tBodyGyro-stdY", "V126" = "tBodyGyro-stdZ",
-                                                "V161" = "tBodyGyroJerk-meanX", "V162" = "tBodyGyroJerk-meanY", "V163" = "tBodyGyroJerk-meanZ",
-                                                "V164" = "tBodyGyroJerk-stdX", "V165" = "tBodyGyroJerk-stdY", "V166" = "tBodyGyroJerk-stdZ",
-                                                "V201" = " tBodyAccMagmean", "V202" = " tBodyAccMagstd", "V214" = "tGravityAccMagmean", "V215" = "tGravityAccMagstd",
-                                                "V227" = "tBodyAccJerkMag-mean", "V228" = "tBodyAccJerkMag-std", "V240" = "tBodyGyroMag-mean", "V241" = "tBodyGyroMag-std",
-                                                "V253" = "tBodyGyroJerkMag-mean", "V254" = "tBodyGyroJerkMag-std", "V266" = "fBodyAcc-meanX", "V267" = "fBodyAcc-meanY",
-                                                "V268" = "fBodyAcc-meanZ", "V269" = "fBodyAcc-stdX", "V270"= "fBodyAcc-stdY", "V271"= "fBodyAcc-stdZ",
-                                                "V345" = "fBodyAccJerk-meanX", "V346" = "fBodyAccJerk-meanY", "V347" = "fBodyAccJerk-meanZ",
-                                                "V348" = "fBodyAccJerk-stdX", "V349" = "fBodyAccJerk-stdY", "V350" ="fBodyAccJerk-stdZ",
-                                                "V424" = "fBodyGyro-meanX", "V425" = "fBodyGyro-meanY", "V426" = "fBodyGyro-meanZ", "V427" = "fBodyGyro-stdX",
-                                                "V428" = "fBodyGyro-stdY", "V429" = "fBodyGyro-stdZ", "V503" = "fBodyAccMag-mean", "V504" = "fBodyAccMag-std",
-                                                "V516" = "fBodyBodyAccJerkMag-mean", "V517" = "fBodyBodyAccJerkMag-std", "V529" = "fBodyBodyGyroMag-mean",
-                                                "V530" = "fBodyBodyGyroMag-std", "V542" = "fBodyBodyGyroJerkMag-mean", "V543" = "fBodyBodyGyroJerkMag-std"))
+ tableDataScrub <- rename(tableDataScrub, tBodyAccmeanX = V1, tBodyAccmeanY = V2, tBodyAccmeanZ = V3,
+                                          tBodyAccstdX = V4, tBodyAccstdY = V5, tBodyAccstdZ = V6,
+                                          tGravityAccmeanX = V41,tGravityAccmeanY = V42, tGravityAccmeanZ = V43,
+                                          tGravityAccstdX = V44, tGravityAccstdY = V45, tGravityAccstdZ = V46,
+                                          tBodyAccJerkmeanX = V81, tBodyAccJerkmeanY = V82, tBodyAccJerkmeanZ = V83,
+                                          tBodyAccJerkstdX = V84, tBodyAccJerkstdY = V85, tBodyAccJerkstdZ = V86,
+                                          tBodyGyromeanX = V121, tBodyGyromeanY = V122, tBodyGyromeanZ = V123,
+                                          tBodyGyrostdX = V124,tBodyGyrostdY = V125, tBodyGyrostdZ = V126,
+                                          tBodyGyroJerkmeanX = V161, tBodyGyroJerkmeanY = V162, tBodyGyroJerkmeanZ = V163,
+                                          tBodyGyroJerkstdX = V164, tBodyGyroJerkstdY = V165, tBodyGyroJerkstdZ = V166,
+                                          tBodyAccMagmean = V201, tBodyAccMagstd = V202, GravityAccMagmean = V214, tGravityAccMagstd = V215,
+                                          tBodyAccJerkMagmean = V227, tBodyAccJerkMagstd = V228, tBodyGyroMagmean = V240, tBodyGyroMagstd = V241,
+                                          tBodyGyroJerkMagmean = V253, tBodyGyroJerkMagstd = V254, fBodyAccmeanX = V266,fBodyAccmeanY = V267,
+                                          fBodyAccmeanZ = V268, fBodyAccstdX = V269, fBodyAccstdY = V270, fBodyAccstdZ = V271,
+                                          fBodyAccJerkmeanX = V345, fBodyAccJerkmeanY = V346, BodyAccJerkmeanZ = V347,
+                                          fBodyAccJerkstdX = V348, fBodyAccJerkstdY = V349, fBodyAccJerkstdZ = V350,
+                                          fBodyGyromeanX = V424, fBodyGyromeanY = V425, fBodyGyromeanZ = V426, fBodyGyrostdX = V427,
+                                          fBodyGyrostdY = V428, fBodyGyrostdZ = V429, fBodyAccMagmean = V503, fBodyAccMagstd = V504,
+                                          fBodyBodyAccJerkMagmean = V516, fBodyBodyAccJerkMagstd = V517, fBodyBodyGyroMagmean = V529,
+                                          fBodyBodyGyroMagstd = V530, fBodyBodyGyroJerkMagmean = V542, fBodyBodyGyroJerkMagstd = V543)
   
   
-  tableDataScrubTwo$TestSubjectID
+ 
   
    
     
@@ -92,6 +98,8 @@ run_analysis <- function()
 #Independent tidy data set with the average of each variable for each activity and each subject
   
   
-  
-  
+tableDataScrub <- arrange(tableDataScrub, TestSubjectID, Activity) #arrange data by subject Id and actvity
+testSubject <- group_by(tableDataScrub, TestSubjectID,Activity) #group the activity by subject ID
+tidyData <- summarize(testSubject, ActivityAverage = mean(tBodyAccmeanX : fBodyBodyGyroJerkMagstd))
+write.table(tidyData, file = "tidydata.txt",sep = "  ", row.names = FALSE) #print the results to the tidyData.txt file 
 }
